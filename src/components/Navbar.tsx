aa2c7429cf1dd133502e45e04b0a4d7f9859c147
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-25 18:06:46 +0300

    navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,18 @@
-import { Pets } from '@mui/icons-material';
-import { AppBar, Box, Toolbar, Typography, styled } from '@mui/material';
+import { Mail, Notifications, Pets } from '@mui/icons-material';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  Fade,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  styled,
+} from '@mui/material';
+import { useState } from 'react';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
@@ -9,15 +22,38 @@ const StyledToolbar = styled(Toolbar)({
 const Search = styled('div')(({ theme }) => ({
   backgroundColor: 'white',
   padding: '0 10px',
-  border: theme.shape.borderRadius,
+  borderRadius: theme.shape.borderRadius,
   width: '40%',
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  backgroundColor: 'white',
+  display: 'none',
+  gap: '20px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'flex',
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: '10px',
+  alignItems: 'center',
+  [theme.breakpoints.up('sm')]: {
+    display: 'none',
+  },
 }));
 
 const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -25,9 +61,44 @@ const Navbar = () => {
           Lama dev
         </Typography>
         <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
-        <Search>search</Search>
-        <Icons>icons</Icons>
+        <Search>
+          <InputBase placeholder="search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://randomuser.me/api/portraits/men/17.jpg"
+            onClick={handleClick}
+          />
+        </Icons>
+        <UserBox onClick={handleClick}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://randomuser.me/api/portraits/men/17.jpg"
+          />
+        </UserBox>
       </StyledToolbar>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={open}
+        onClose={handleClose}
+        anchorEl={anchorEl}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
