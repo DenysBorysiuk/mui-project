1cd00408ad8f278857ade960909ddea0aa8ba400
Denys Borysiuk <denisborisiuk@gmail.com>
2023-10-25 11:36:12 +0300

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
 import { Pets } from '@mui/icons-material';
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, styled } from '@mui/material';
 
 const StyledToolbar = styled(Toolbar)({
   display: 'flex',
   justifyContent: 'space-between',
 });
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  border: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  backgroundColor: 'white',
+}));
 
 const Navbar = () => {
   return (
@@ -14,6 +25,8 @@ const Navbar = () => {
           Lama dev
         </Typography>
         <Pets sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>search</Search>
+        <Icons>icons</Icons>
       </StyledToolbar>
     </AppBar>
   );
